1f1ccb8f1cfeacd4418141e253e56f8aecc7df5c
Knarkzel <knarkzel@gmail.com>
2023-07-06 18:03:25 +0200

    Add rust-src

--- flake.nix
+++ flake.nix
@@ -2,18 +2,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+      overlays = [ rust-overlay.overlays.default ];
+      pkgs = import nixpkgs {inherit system overlays;};
     in {
       devShell = pkgs.mkShell {
         buildInputs = [
           pkgs.just
-          pkgs.cargo
           pkgs.diesel-cli
           pkgs.nodePackages.npm
+          (pkgs.rust-bin.stable.latest.default.override {
+            extensions = ["rust-src"];
+          })
         ];
       };
     });
